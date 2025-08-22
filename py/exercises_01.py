1dcd94b1a6dd1bf434c43f4780da666dc4c27a63
Jair171676 <143846363+Jair171676@users.noreply.github.com>
2025-08-21 19:44:57 -0600

    Update exercises_01.py

--- py/exercises_01.py
+++ py/exercises_01.py
@@ -5,18 +5,16 @@
 # orden apellido paterno, apellido materno y nombres, ejemplo: Revilla Silva Jesús Alberto
 # comienza cada nombre con mayúscuala y el resto con minúsculas incluye acentos y otros
 # caracteres del Español.
-# Escribe el código aquí
 nombre='Nieto Diaz de Leon Jair de Jesus'
 
 
 # 1.1 Declara una variable matricula y asigna tu número de matrícula como entero
-# Escribe el código aquí
 matricula=171676
 
 
 # 2. Crear una función que sume dos números
 def sumar(a, b):
- return(a+b)
+return(a+b)
 # reemplaza pass con tu código
 
 
@@ -25,7 +23,7 @@ def sumar(a, b):
 def bucle_for():
     for i in range (1,10):
         display.scroll(i)
-display.scroll(bucle_for())
+print(bucle_for())
     
 
 
@@ -34,6 +32,6 @@ def print_numbers():
     numbers = [1, 2, 3, 4, 5]
     i=0
     while i<4:
-        display.scroll(numbers[i])
+        print(numbers[i])
         i=i+1
     # Escribe el código aquí
