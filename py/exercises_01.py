29909774c9d4757232f37f4575a99c7b57708fea
Jair171676 <143846363+Jair171676@users.noreply.github.com>
2025-08-29 19:12:17 -0600

    Update exercises_01.py

--- py/exercises_01.py
+++ py/exercises_01.py
@@ -31,6 +31,6 @@ def print_numbers():
     numbers = [1, 2, 3, 4, 5]
     i=0
     while i<4:
-        print(numbers[i])
+        print(numbers[i] + "\n")
         i+=1
     # Escribe el código aquí
